f7d01a98708542ad8235c52c1f73f0e5a2a1e1b7
Geon Lee <drik86421@gmail.com>
2024-01-02 18:26:27 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/me/whiteship/restapiwithspring/RestApiWithSpringApplication.java
@@ -0,0 +1,14 @@
+package me.whiteship.restapiwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringApplication {
+
+    public static void main(String[] args) {
+        //
+        SpringApplication.run(RestApiWithSpringApplication.class, args);
+    }
+
+}
