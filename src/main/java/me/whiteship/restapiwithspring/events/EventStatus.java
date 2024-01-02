88f2feb8b0f4e8e78febd64cafe55a0290ac7113
Geon Lee <drik86421@gmail.com>
2024-01-02 19:24:38 +0900

    feat: Event Domain feat

---
+++ src/main/java/me/whiteship/restapiwithspring/events/EventStatus.java
@@ -0,0 +1,7 @@
+package me.whiteship.restapiwithspring.events;
+
+public enum EventStatus {
+    DRAFT,
+    PUBLISHED,
+    BEGAN_ENROLLMENT
+}
---
+++ src/test/java/me/whiteship/restapiwithspring/events/EventTest.java
@@ -0,0 +1,34 @@
+package me.whiteship.restapiwithspring.events;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        //Given
+        String name = "Event";
+        String spring = "Spring";
+
+        // When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(spring);
+
+        //Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(spring);
+    }
+}
