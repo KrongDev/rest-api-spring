51fa893fea0ad5aa9be2fc4e9404d22b75247b80
Geon Lee <drik86421@gmail.com>
2024-01-14 05:28:52 +0900

    POST /api/events returns 201

---
+++ src/main/java/me/whiteship/restapiwithspring/events/EventController.java
@@ -0,0 +1,27 @@
+package me.whiteship.restapiwithspring.events;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+    @Autowired
+    EventRepository eventRepository;
+    @PostMapping("")
+    public ResponseEntity<?> createEvent(@RequestBody Event event) {
+        //error 발생으로 인한 이슈 version 문제로 보임
+        Event newEvent = this.eventRepository.save(event);
+        URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        return ResponseEntity.created(createUri).body(event);
+    }
+}
--- src/test/java/me/whiteship/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/me/whiteship/restapiwithspring/events/EventControllerTests.java
@@ -1,15 +1,23 @@
 package me.whiteship.restapiwithspring.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.bytebuddy.asm.Advice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,16 +27,38 @@ public class EventControllerTests {
     //Spring mvc에 대한 tes 디스팩처 서블릿 생성후 Bean들을 테스트 단위테스트보다 걸린다.
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
 
+    //repository를 bean으로 생성할 때 사용
+    @MockBean
+    EventRepository eventRepository;
     // api.json api.xml 확장자 요청 -> 보다는-> accept header지정을 추천
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11,  25, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 25, 14, 21 ))
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
         //perform 요청
-        mockMvc.perform(post("/api/events/")
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event))
                 )
+                .andDo(print())
                 //create는 201
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
